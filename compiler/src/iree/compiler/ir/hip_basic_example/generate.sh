f0e47b5883514501f683d2ccf571e843c6ca660f
James Newling <james.newling@gmail.com>
2025-08-23 13:02:52 -0700

    make the example test numerics

--- compiler/src/iree/compiler/ir/hip_basic_example/generate.sh
+++ compiler/src/iree/compiler/ir/hip_basic_example/generate.sh
@@ -6,5 +6,5 @@ hipcc  -emit-llvm -S --offload-arch=gfx942 --std=c++17 main.hip
 
 #  Below, the use of the -g flag was suggested in
 #  https://rocm.blogs.amd.com/software-tools-optimization/amdgcn-isa/README.html
-echo "Generating asm and executable"
+echo "Generating assembly (asm/s) and an executable program"
 hipcc -save-temps=obj -g --offload-arch=gfx942 --std=c++17 main.hip -o ${PWD}/main
